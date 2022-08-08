bbfaa2ab41c03728246c85e3ff2f5108e8d807cd
moshmatohead <xaviergabriel856@gmail.com>
2022-08-08 16:58:57 -0300

    teste1

---
+++ index.js
@@ -0,0 +1,21 @@
+let a = 1;
+let b = 3;
+let soma = a+b;
+
+const express = require('express');
+const app = express();
+
+console.log(soma);
+
+let userDB = [
+    {
+        nome: 'Xavier',
+        idade: 17,
+    },
+    {
+        nome: 'Gabriel',
+        idade: 18,
+    }
+]
+
+console.log(userDB);
